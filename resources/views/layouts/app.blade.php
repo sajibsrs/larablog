dfa279c6fc4b03f5662d9877cc47f23c509d010c
Sajidur Rahman <sajibsrs@gmail.com>
2020-03-26 03:35:25 +0600

    users index link added to navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -33,7 +33,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li><a class="nav-link" href="{{ route('users.index') }}">Users</a></li>
+                        <li><a class="nav-link" href="#">Posts</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
