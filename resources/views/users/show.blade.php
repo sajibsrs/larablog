0d5f249fec6a03a59a90d98c9844919453e43b43
Sajidur Rahman <sajibsrs@gmail.com>
2020-03-26 04:07:43 +0600

    added heading and spacing adjusted

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,6 +4,12 @@
     <div class="container">
         <div class="row">
             <div class="col">
+                <h1 class="h2">User Profile</h1>
+                <hr class="mb-4">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
                 <div class="media">
                     <svg class="rounded-circle mr-3" width="75" height="75" xmlns="http://www.w3.org/2000/svg"
                          preserveAspectRatio="xMidYMid slice" focusable="false" role="img"
@@ -12,8 +18,9 @@
                         <rect width="100%" height="100%" fill="#868e96"></rect>
                     </svg>
                     <div class="media-body">
-                        <h5 class="mt-0">{{ $user->name }}</h5>
-                        {{ $user->email }}
+                        <h5 class="mt-0 mb-1">{{ $user->name }}</h5>
+                        <p class="mb-1">{{ $user->email }}</p>
+                        <p class="mb-1">{{ $user->created_at }}</p>
                     </div>
                 </div>
             </div>
