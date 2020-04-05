619a0d51b16a906355039fbd686121d023cb4799
Sajidur Rahman <sajibsrs@gmail.com>
2020-04-05 23:04:17 +0600

    trix js added

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,1 +1,20 @@
-edit
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="h2">All posts</h1>
+                <hr class="mb-4">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <form>
+                    <input id="x" type="hidden" name="content">
+                    <trix-editor input="x"></trix-editor>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
