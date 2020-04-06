ddba70c8afcb371b9687f50b24f412459d02746b
Sajidur Rahman <sajibsrs@gmail.com>
2020-04-06 08:26:23 +0600

    added highlight js for rich tex code

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,9 @@
 require('./bootstrap');
 require('trix');
+const hljs = require('highlight.js');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('pre').forEach((block) => {
+      hljs.highlightBlock(block);
+    });
+});
