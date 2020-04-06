7f062242c8c47582be88426b42f7f357376dd7db
Sajidur Rahman <sajibsrs@gmail.com>
2020-04-06 07:12:42 +0600

    added post edit form and populated with content

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -4,15 +4,26 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1 class="h2">All posts</h1>
+                <h1 class="h2">Edit post</h1>
                 <hr class="mb-4">
             </div>
         </div>
         <div class="row">
             <div class="col">
-                <form>
-                    <input id="x" type="hidden" name="content">
-                    <trix-editor input="x"></trix-editor>
+                <form action="POST" action="/posts">
+                    @csrf
+                    <div class="form-group">
+                        <label for="title">Post title</label>
+                        <input type="text" class="form-control" value="{{ $post->title }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="content">Post content</label>
+                        <input id="content" type="hidden" name="content" value="{{ $post->content }}">
+                        <trix-editor input="content"></trix-editor>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value="Submit" class="btn btn-primary">
+                    </div>
                 </form>
             </div>
         </div>
